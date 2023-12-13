6f35f0b7b211ba2c2b77ff798201ff337589f99f
yk-graph <yokokura.tatsuya@gmail.com>
2023-12-13 17:22:02 +0900

    fix: users page

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,4 +1,4 @@
-export function UsersPage() {
+export default function UsersPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       This is Users Page
