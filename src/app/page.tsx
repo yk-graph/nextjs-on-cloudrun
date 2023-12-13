8b434a9aed3f1e30348ed0d5d4e4c06d9ee7a21b
yk-graph <yokokura.tatsuya@gmail.com>
2023-12-14 07:35:32 +0900

    test env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+console.log("DATABASE_URL", process.env.DATABASE_URL);
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       This is Test Page
+      <p>DATABASE_URL is... {process.env.DATABASE_URL}</p>
     </main>
   );
 }
