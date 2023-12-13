6eefe2ae5b2c1cb4a36e3f6c5319b996ff492d82
yk-graph <yokokura.tatsuya@gmail.com>
2023-12-13 20:40:39 +0900

    add: axios

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,20 @@
-export default function UsersPage() {
+export default async function UsersPage() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  if (!res.ok) {
+    return <div>Failed to load</div>;
+  }
+
+  const users = await res.json();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       This is Users Page
+      <ul>
+        {users.map((user: any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </main>
   );
 }
