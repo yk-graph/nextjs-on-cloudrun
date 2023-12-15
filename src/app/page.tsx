a6dc0df967a0f2f7d0f083f8e06b1d1d70eeb45e
yk-graph <yokokura.tatsuya@gmail.com>
2023-12-16 00:07:33 +0900

    reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-console.log("DATABASE_URL", process.env.DATABASE_URL);
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
